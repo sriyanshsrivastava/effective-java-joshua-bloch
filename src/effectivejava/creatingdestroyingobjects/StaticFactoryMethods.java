4c44323b2a23c7d4c51f6c4860ab973c5032c445
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2026-01-02 15:54:13 +0530

    initial commit

---
+++ src/effectivejava/creatingdestroyingobjects/StaticFactoryMethods.java
@@ -0,0 +1,7 @@
+package effectivejava.creatingdestroyingobjects;
+
+public class StaticFactoryMethods {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
